d6e7f4e735bca9d355291b3d1b832f10c2ccfc61
Robijn Noort <rrnoort@gmail.com>
2022-09-30 14:14:19 +0200

    feat: added two pages for registering for the openday

---
+++ app/views/register_openday/index.php
@@ -0,0 +1,9 @@
+<h1>Registreren voor de opendag</h1>
+<form method="post" action+"/registeropenday/create">
+    <label for="">Voornaam</label>
+    <input type="text">
+    <br>
+    <label for="">Achternaam    </label>
+    <input type="text">
+    
+</form>
