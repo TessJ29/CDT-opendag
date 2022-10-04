aae89ff0a1b2190172b4c9c22a7606659d4b86f4
Tess Jansen <tess.jansen29@gmail.com>
2022-10-04 13:19:15 +0200

    course pagina met styling

--- app/views/course.php
+++ app/views/course.php
@@ -107,7 +107,7 @@
                 <div class="cardtitle1">Software developer</div>
                 <div class="line"></div>
                 <div class="course-information">
-                    <button onclick="show('data-SD')">
+                    <button data-toggle="dropdown" data-target="#data-SD">
                         <a>Meer lezen</a>
                         <ion-icon name="arrow-forward-outline" id="rotated-icon"></ion-icon>
                     </button>
@@ -118,7 +118,7 @@
                 <div class="cardtitle2">Expert IT systems and devices</div>
                 <div class="line"></div>
                 <div class="course-information">
-                    <button onclick="show('data-ED')">
+                    <button data-toggle="dropdown" data-target="#data-ED">
                         <a>Meer lezen</a>
                         <ion-icon name="arrow-forward-outline"></ion-icon>
                     </button>
@@ -129,7 +129,7 @@
                 <div class="cardtitle1">Medewerker ICT support</div>
                 <div class="line"></div>
                 <div class="course-information">
-                    <button onclick="show('data-MI')">
+                    <button data-toggle="dropdown" data-target="#data-MI">
                         <a>Meer lezen</a>
                         <ion-icon name="arrow-forward-outline"></ion-icon>
                     </button>
@@ -140,7 +140,7 @@
                 <div class="cardtitle2">Allround medewerker IT systems and devices</div>
                 <div class="line"></div>
                 <div class="course-information">
-                    <button onclick="show('data-AD')">
+                    <button data-toggle="dropdown" data-target="#data-AD">
                         <a>Meer lezen</a>
                         <ion-icon name="arrow-forward-outline"></ion-icon>
                     </button>
@@ -148,58 +148,67 @@
             </div>
         </div>
     </section>
-
-    <div class="information">
-        <div class="grid">
-            <data id="data-SD">
-                <div class="grid-item-1">
+    <section class="course_information">
+        <div class="info">
+            <div id="data-SD" class="dropdown-menu">
+                <div class="content-left">
                     <ion-icon name="code-slash-outline" size="large"></ion-icon>
                 </div>
-                <div class="grid-item-2">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex tempora similique blanditiis, nostrum quia asperiores cupiditate repellat explicabo consequatur? Earum doloribus error blanditiis enim vitae at nesciunt unde eius porro.</p>
+                <div class="content-right">
+                    <h3>Software Developer</h3>
+                    <p>Tijdens deze drie tot vier-jarige opleiding leer je hoe je zelfstandig apps, games of webapplicaties kunt maken met behulp van frameworks en object georiënteerd programmeren.
+                         Het is een echte programmeeropleiding, geen knippen en plakken maar degelijk codekloppen.
+                         Je werkt projectmatig en gebruikt programmeertalen zoals PHP, C#, Javascript en SQL. Je ontwikkelt je beroepshouding, presentatietechnieken 
+                         en leert samen te werken. Je leert een product ontwikkelen, bouwen, testen, documenteren en opleveren.</p>
                     <p>NIVEAU 4 BOL 3-4 JR</p>
                 </div>
-            </data>
+            </div>
         </div>
         <div class="info">
-            <data id="data-ED">
-                <div class="grid-item-1">
+            <div id="data-ED" class="dropdown-menu">
+                <div class="content-left">
                     <ion-icon name="code-slash-outline" size="large"></ion-icon>
                 </div>
-                <div class="grid-item-2">
-                    <p>ED.</p>
+                <div class="content-right">
+                    <h3>Expert IT systems and devices</h3>
+                    <p>Als Expert IT systems and devices ben jij er verantwoordelijk voor dat alle collega’s hun werk op een goede manier kunnen doen. Jij houd je vooral bezig met service, beheer en veiligheid van verschillende netwerken en servers. Jij weet hoe de verschillende apparaten programma’s en besturingssystemen werken (bijvoorbeeld Windows, Linux, Android en macOS). 
+                        Wanneer iemand jou een vraag stelt over zijn pc, Ipad, mobiel, internet, server of programma, kun jij ze verder helpen. Ook weet je (straks) wat cloudcomputing is.
+                        Je probeert problemen te voorkomen. En als er dan toch een probleem is, ga jij meteen op zoek naar een oplossing. Het is ook jouw taak om het bedrijf goed te kennen.
+                        Uiteraard moet je je steeds verdiepen in nieuwe ontwikkelingen op het gebied van ICT. Hoe kun je nieuwe technologieën toepassen? Hoe blijf je hackers een stap voor? Daarnaast geef je ook adviezen over hoe de ICT security kan worden verbeterd. Jij bent de specialist.
+                        Jij leert, ook onder stress, jouw medewerkers op de ICT-afdeling instructies te geven en te motiveren.</p>
                     <p>NIVEAU 4 BOL 3-4 JR</p>
                 </div>
-            </data>
+            </div>
         </div>
         <div class="info">
-            <data id="data-MI">
-                <div class="grid-item-1">
+            <div id="data-MI" class="dropdown-menu">
+                <div class="content-left">
                     <ion-icon name="code-slash-outline" size="large"></ion-icon>
                 </div>
-                <div class="grid-item-2">
-                    <p>MI</p>
-                    <p>NIVEAU 4 BOL 3-4 JR</p>
+                <div class="content-right">
+                    <h3>Medewerker ICT support</h3>
+                    <p>Tijdens de opleiding leer je van alles wat met ICT en Elektro te maken heeft: elektro-installatietechniek, datanetwerken, domotica, software, en hoe een computer in elkaar zit. 
+                        Ook ontwikkel je tijdens je studie vaardigheden om andere computergebruikers te kunnen helpen. We besteden aandacht aan de nieuwste technologieën op het gebied van elektro en ICT.
+                        Als je een techneut bent en graag met je handen werkt, kun je als medewerker ICT alle kanten uit. Waar je ook voor kiest: jij zorgt ervoor dat ICT-voorzieningen goed blijven werken en helpt gebruikers als ze er problemen mee hebben.</p>
+                    <p>NIVEAU 2 BOL 1 JR</p>
                 </div>
-            </data>
+            </div>
         </div>
         <div class="info">
-            <data id="data-AD">
-                <div class="grid-item-1">
+            <div id="data-AD" class="dropdown-menu">
+                <div class="content-left">
                     <ion-icon name="code-slash-outline" size="large"></ion-icon>
                 </div>
-                <div class="grid-item-2">
-                    <p>AD</p>
-                    <p>NIVEAU 4 BOL 3-4 JR</p>
+                <div class="content-right">
+                    <h3>Allround medewerker IT systems and devices</h3>
+                    <p>Als Allround medewerker IT systems and devices zorg jij ervoor dat je collega werknemers in een bedrijf hun werk kunnen doen. Zonder laptop, internet, e-mail, telefoon en werkende programma’s kan niemand werken. Jij zorgt ervoor dat het netwerk snel en betrouwbaar is. 
+                        Gelukkig hoef je dat niet in je eentje te doen. Je werkt soms alleen, maar vaak ook in een team.
+                        Je werkt vooral aan het helpen van gebruikers en het installeren en configureren van allerlei programma’s en apparaten. 
+                        Het maakt voor jou niet veel uit of iemand een vraag heeft over zijn laptop, tablet, telefoon, internet of een ingewikkeld programma.  Iedere vraag is voor jou een leuke uitdaging. Ook geef je gebruikers uitleg en instructies, zodat zij goed met hun hardware en programma’s kunnen werken.</p>
+                    <p>NIVEAU 3 BOL 2 JR</p>
                 </div>
-            </data>
+            </div>
         </div>
-        </nav>
-
-        <!-- SD -->
-
-
-    </div>
     </section>
 
     <!-- Footer -->
@@ -290,13 +299,34 @@
         //     }
 
         // Dropdown
-        let show = id => {
-            let dropElement = document.getElementById(id);
-            if (dropElement.style.display === "none") {
-                dropElement.style.display = "block";
-            } else {
-                dropElement.style.display = "none";
-            }
+        // let show = id => {
+        //     let dropElement = document.getElementById(id);
+        //     if (dropElement.style.display === "none") {
+        //         dropElement.style.display = "block";
+        //     } else {
+        //         dropElement.style.display = "none";
+        //     }
+        // }
+        var list = document.querySelectorAll("[data-toggle=dropdown]");
+        for (var i = 0, s = list.length; i < s; i++) {
+            var elm = list[i];
+            elm.addEventListener("click", function() {
+                // this for loop will hide previously clicked drop downs
+                for (var j = 0, z = list.length; j < z; j++) {
+                    if (list[j] != this) {
+                        var elm = document.querySelector(list[j].getAttribute("data-target"));
+                        var str = elm.className.replace("d-block");
+                        elm.className = str;
+                    }
+                } // if you like, remove the above loop
+                var obj = document.querySelector(this.getAttribute("data-target"));
+                if (obj.className.indexOf("d-block") > 0) {
+                    var temp = obj.className.replace("d-block", "");
+                    obj.className = temp;
+                } else {
+                    obj.className += " d-block";
+                }
+            });
         }
     </script>
     <!-- Icon Javascripts -->
