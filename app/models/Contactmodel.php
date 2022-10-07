1b7c07242c2c2bc0166bc1c313acc54759a1f2e8
Tess Jansen <tess.jansen29@gmail.com>
2022-10-07 14:03:19 +0200

    Added code to insert data in contactform table

--- app/models/Contactmodel.php
+++ app/models/Contactmodel.php
@@ -8,4 +8,17 @@ class Contactmodel
     {
         $this->db = new Database();
     }
+
+    public function createContactFormRow ()
+    {
+        $this->db->query('INSERT INTO `contactform` (`FirstName`, `LastName`, `Email`, `PhoneNumber`, `Message`) VALUES (:FirstName, :LastName, :Email, :PhoneNumber, :Message)');
+        $this->db->bind(':FirstName', $_POST['FirstName']);
+        $this->db->bind(':LastName', $_POST['LastName']);
+        $this->db->bind(':Email', $_POST['Email']);
+        $this->db->bind(':PhoneNumber', $_POST['PhoneNumber']);
+        $this->db->bind(':Message', $_POST['Message']);
+        $this->db->execute();
+
+        header('Location: ../views/contact');
+    }
 }
