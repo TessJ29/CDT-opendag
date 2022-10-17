529bf7ac65e264333c336d8977f17e5ecba1f047
Robijn Noort <rrnoort@gmail.com>
2022-10-17 14:06:06 +0200

    feat: added getAll function for openday registration

--- app/models/OpendayRegistration.php
+++ app/models/OpendayRegistration.php
@@ -10,14 +10,20 @@
 
         public function create($data)
         {
-            var_dump($data);
-            $this->db->query("INSERT INTO `opendayregistration` (`id`, `firstname`, `lastname`, `email`, `phone`) VALUES (NULL, ':firstname', ':lastname', ':email', ':phone');");
+            $this->db->query("INSERT INTO `opendayregistration` (`id`, `firstname`, `lastname`, `email`, `phone`) VALUES (NULL, :firstname, :lastname, :email, :phone);");
             $this->db->bind(":firstname", $data["firstname"], PDO::PARAM_STR);
             $this->db->bind(":lastname", $data["lastname"], PDO::PARAM_STR);
             $this->db->bind(":email", $data["email"], PDO::PARAM_STR);
             $this->db->bind(":phone", $data["phone"], PDO::PARAM_STR);
 
             $this->db->execute();
+        }
+
+        public function getAll()
+        {
+            $this->db->query("SELECT * FROM `opendayregistration`;");
+            $result = $this->db->resultSet();
+            return $result;
         }
     }
 ?>
