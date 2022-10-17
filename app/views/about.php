80e9a58e30b5c9ab8e0d6fddb3fd59799de10ed1
Tess Jansen <tess.jansen29@gmail.com>
2022-10-17 14:19:34 +0200

    styling and html for about page

--- app/views/about.php
+++ app/views/about.php
@@ -1,3 +1,194 @@
-<?php
-    echo "d";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- External stylesheet -->
+    <link rel="stylesheet" href="../public/scss/main.css">
+
+    <title>MBO Utrecht opendag</title>
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            <div class="top__navbar">
+                <ul class="navbar__logo">
+                    <li><img src="./img/mbologo.png" alt=""></li>
+                </ul>
+                <div class="navbar__toggle" id="mobile-menu">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>
+                <ul class="navbar__contact--links">
+                    <li>
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <a href="">info@mboutrecht.nl</a>
+                    </li>
+                    <li>
+                        <ion-icon name="logo-whatsapp"></ion-icon>
+                        <a href="">06 - 257 050 51 (WhatsApp)</a>
+                    </li>
+                </ul>
+                <ul class="navbar__media--links">
+                    <li>
+                        <a class="navbar__media--link" href="">
+                            <ion-icon name="logo-linkedin"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="navbar__media--link" href="">
+                            <ion-icon name="logo-twitter"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="navbar__media--link" href="">
+                            <ion-icon name="logo-youtube"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="navbar__media--link" href="">
+                            <ion-icon name="logo-instagram"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="navbar__media--link" href="">
+                            <ion-icon name="logo-facebook"></ion-icon>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="bottom__navbar">
+                <ul>
+                    <li>
+                        <a href="./index.php">HOME</a>
+                    </li>
+                    <li>
+                        <a class="active" href="/about">OVER MBO UTRECHT</a>
+                    </li>
+                    <li>
+                        <a href="/course">OPLEIDINGEN</a>
+                    </li>
+                    <li>
+                        <a href="/contact">CONTACT</a>
+                    </li>
+                    <li>
+                        <a href="/workshop">VOORLICHTINGEN</a>
+                    </li>
+
+                </ul>
+                <button>
+                    <a href="">Enquette</a>
+                    <ion-icon name="arrow-forward-outline"></ion-icon>
+                </button>
+            </div>
+        </nav>
+    </header>
+    <section class="about_landing">
+        <h1>Over MBO Utrecht</h1>
+        <div>
+            <img src="" alt="">
+        </div>
+        <div class="grid-container">
+            <div class="grid-item">
+                <h4>MBO Utrecht</h4>
+                <p>MBO Utrecht wil jou helpen in de ontwikkeling van student tot zelfbewuste professional. Wij geven jou meer dan een opleiding en leren je meer dan een vak. Er is veel aandacht voor de ontwikkeling van talent, arbeidsvaardigheden, ondernemend gedrag, maatschappelijke betrokkenheid, respect voor elkaar en burgerschap. </p>
+            </div>
+            <div class="grid-item">
+                <h4>Kleinschalige school en betrokken docenten</h5>
+                <p>Bij MBO Utrecht kun je op goed bereikbare locaties wel 50 verschillende opleidingen volgen. Wij zijn een kleinschalige school waar jij je snel thuis zult voelen. Dat komt door onze betrokken docenten die niet alleen aandacht hebben voor jou als persoon, maar ook voor de manier waarop jij leert.</p>
+            </div>
+            <div class="grid-item">
+                <p>MBO Utrecht is een kleinschalige, interconfessionele onderwijsinstelling die doelmatig onderwijs aanbiedt met focus op de hoogste niveaus van het mbo. We bieden met ca. 500 medewerkers ruim 50 mbo-opleidingen aan ongeveer 5.000 studenten, in uiteenlopende de domeinen van het mbo-onderwijs.</p>
+            </div>
+            <div class="grid-item">
+                <h4>Ruimte voor maatwerk</h5>
+                <p>Er is veel ruimte voor maatwerk. We hebben bijvoorbeeld veel doorstroom- en versnellingsmogelijkheden. Maar er is ook ondersteuning wanneer dat nodig is.</p>
+            </div>
+            <div class="grid-iteminvis"></div>
+            <div class="grid-item">
+                <h4>Straks een goed opgeleide professional</h5>
+                <p>Bij ons word je niet alleen opgeleid voor een vak. We vinden het belangrijk dat je naast je opleiding ook vaardigheden leert die je helpen in deze wereld die continu verandert. Zo ben jij straks een goed opgeleide professional die klaar is om in de praktijk aan de slag te gaan.</p>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="footer__top--container">
+            <div class="footer__top">
+                <div>
+                    <h3>Volg ons</h3>
+                    <ul>
+                        <li>
+                            <ion-icon name="logo-facebook"></ion-icon>
+                            <p>Facebook</p>
+                        </li>
+                        <li>
+                            <ion-icon name="logo-instagram"></ion-icon>
+                            <p>Instagram</p>
+                        </li>
+                        <li>
+                            <ion-icon name="logo-youtube"></ion-icon>
+                            <p>YouTube</p>
+                        </li>
+                        <li>
+                            <ion-icon name="logo-twitter"></ion-icon>
+                            <p>Twitter</p>
+                        </li>
+                        <li>
+                            <ion-icon name="logo-linkedin"></ion-icon>
+                            <p>LinkedIn</p>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>
+                            <ion-icon name="location-outline"></ion-icon>
+                            <p>Daltonlaan 300 3584 BK Utrecht</p>
+                        </li>
+                        <li>
+                            <ion-icon name="call-outline"></ion-icon>
+                            <p>03 - 28 15 100</p>
+                        </li>
+                        <li>
+                            <ion-icon name="logo-whatsapp"></ion-icon>
+                            <p>06 - 257 050 51 (WhatsApp)</p>
+                        </li>
+                        <li>
+                            <ion-icon name="mail-outline"></ion-icon>
+                            <p>info@mboutrecht.nl</p>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Snel naar</h3>
+                    <ul>
+                        <li>
+                            <p><a href="">Homepagina</a></p>
+                        </li>
+                        <li>
+                            <p><a href="">Opleidingen</a></p>
+                        </li>
+                        <li>
+                            <p><a href="">Inschrijven voor workshop</a></p>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                </div>
+            </div>
+        </div>
+        <div class="footer__bottom">
+            <p>MBO Utrecht ICT Academie 2022. All rights reserved.</p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
