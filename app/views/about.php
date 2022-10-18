48ee0ac5a392ccd932398f8509d6ea68ad8e27c4
Robijn Noort <rrnoort@gmail.com>
2022-10-18 14:54:14 +0200

    feat: added navbar and footer by using partials

--- app/views/about.php
+++ app/views/about.php
@@ -13,81 +13,9 @@
 
 <body>
 
-    <header>
-        <nav>
-            <div class="top__navbar">
-                <ul class="navbar__logo">
-                    <li><img src="./img/mbologo.png" alt=""></li>
-                </ul>
-                <div class="navbar__toggle" id="mobile-menu">
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                </div>
-                <ul class="navbar__contact--links">
-                    <li>
-                        <ion-icon name="mail-outline"></ion-icon>
-                        <a href="">info@mboutrecht.nl</a>
-                    </li>
-                    <li>
-                        <ion-icon name="logo-whatsapp"></ion-icon>
-                        <a href="">06 - 257 050 51 (WhatsApp)</a>
-                    </li>
-                </ul>
-                <ul class="navbar__media--links">
-                    <li>
-                        <a class="navbar__media--link" href="">
-                            <ion-icon name="logo-linkedin"></ion-icon>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="navbar__media--link" href="">
-                            <ion-icon name="logo-twitter"></ion-icon>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="navbar__media--link" href="">
-                            <ion-icon name="logo-youtube"></ion-icon>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="navbar__media--link" href="">
-                            <ion-icon name="logo-instagram"></ion-icon>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="navbar__media--link" href="">
-                            <ion-icon name="logo-facebook"></ion-icon>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="bottom__navbar">
-                <ul>
-                    <li>
-                        <a href="./index.php">HOME</a>
-                    </li>
-                    <li>
-                        <a class="active" href="/about">OVER MBO UTRECHT</a>
-                    </li>
-                    <li>
-                        <a href="/course">OPLEIDINGEN</a>
-                    </li>
-                    <li>
-                        <a href="/contact">CONTACT</a>
-                    </li>
-                    <li>
-                        <a href="/workshop">VOORLICHTINGEN</a>
-                    </li>
+    <!-- Navigation Bar -->
+    <?php $this->view("partials/navbar"); ?>
 
-                </ul>
-                <button>
-                    <a href="">Enquette</a>
-                    <ion-icon name="arrow-forward-outline"></ion-icon>
-                </button>
-            </div>
-        </nav>
-    </header>
     <section class="about_landing">
         <div>
             <img src="../../public/img/overmboimage.png" alt="">
@@ -117,77 +45,8 @@
         </div>
     </section>
 
-    <footer>
-        <div class="footer__top--container">
-            <div class="footer__top">
-                <div>
-                    <h3>Volg ons</h3>
-                    <ul>
-                        <li>
-                            <ion-icon name="logo-facebook"></ion-icon>
-                            <p>Facebook</p>
-                        </li>
-                        <li>
-                            <ion-icon name="logo-instagram"></ion-icon>
-                            <p>Instagram</p>
-                        </li>
-                        <li>
-                            <ion-icon name="logo-youtube"></ion-icon>
-                            <p>YouTube</p>
-                        </li>
-                        <li>
-                            <ion-icon name="logo-twitter"></ion-icon>
-                            <p>Twitter</p>
-                        </li>
-                        <li>
-                            <ion-icon name="logo-linkedin"></ion-icon>
-                            <p>LinkedIn</p>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>Contact</h3>
-                    <ul>
-                        <li>
-                            <ion-icon name="location-outline"></ion-icon>
-                            <p>Daltonlaan 300 3584 BK Utrecht</p>
-                        </li>
-                        <li>
-                            <ion-icon name="call-outline"></ion-icon>
-                            <p>03 - 28 15 100</p>
-                        </li>
-                        <li>
-                            <ion-icon name="logo-whatsapp"></ion-icon>
-                            <p>06 - 257 050 51 (WhatsApp)</p>
-                        </li>
-                        <li>
-                            <ion-icon name="mail-outline"></ion-icon>
-                            <p>info@mboutrecht.nl</p>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>Snel naar</h3>
-                    <ul>
-                        <li>
-                            <p><a href="">Homepagina</a></p>
-                        </li>
-                        <li>
-                            <p><a href="">Opleidingen</a></p>
-                        </li>
-                        <li>
-                            <p><a href="">Inschrijven voor workshop</a></p>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                </div>
-            </div>
-        </div>
-        <div class="footer__bottom">
-            <p>MBO Utrecht ICT Academie 2022. All rights reserved.</p>
-        </div>
-    </footer>
+    <!-- Footer -->
+    <?php $this->view("partials/footer"); ?>
 
 </body>
 
