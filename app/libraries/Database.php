9c3af9764b4d2138ec152e68fd48c8ab610ab882
Tess Jansen <tess.jansen29@gmail.com>
2022-10-21 13:51:55 +0200

    compiled styling

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -1,38 +1,38 @@
 <?php
 
-    class Database
+class Database
+{
+    private $dbHost = DB_HOST;
+    private $dbUser = DB_USER;
+    private $dbPass = DB_PASS;
+    private $dbName = DB_NAME;
+    private $dbHandler;
+
+    public function __construct()
     {
-        private $dbHost = DB_HOST;
-        private $dbUser = DB_USER;
-        private $dbPass = DB_PASS;
-        private $dbName = DB_NAME;
-        private $dbHandler;
+        $conn = "mysql:host=$this->dbHost;dbname=$this->dbName";
 
-        public function __construct()
-        {
-            $conn = "mysql:host=$this->dbHost;dbname=$this->dbName";
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
 
-            $options = array(
-                PDO::ATTR_PERSISTENT => true,
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-            );
-
-            try {
-                $this->dbHandler = new PDO($conn, $this->dbUser, $this->dbPass, $options);
-            } catch(PDOException $e) {
-               echo $e->getMessage();
-            } 
+        try {
+            $this->dbHandler = new PDO($conn, $this->dbUser, $this->dbPass, $options);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
         }
+    }
 
-        public function query($sql)
-        {
-            $this->statement = $this->dbHandler->prepare($sql);
-        }
+    public function query($sql)
+    {
+        $this->statement = $this->dbHandler->prepare($sql);
+    }
 
-        public function bind($param, $value, $type = null)
-        { 
-            switch($value)
-            {
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch ($value) {
                 case is_int($value):
                     $type = PDO::PARAM_INT;
                     break;
@@ -42,31 +42,33 @@
                 case is_null($value):
                     $type = PDO::PARAM_NULL;
                     break;
-                default:                    
+                default:
                     $type = PDO::PARAM_STR;
                     break;
-            } 
-            $this->statement->bindValue($param, $value, $type);
+            }
         }
 
-        public function execute()
-        {
-            return $this->statement->execute();
-        }
-        public function resultSet()
-        {
-            $this->execute();
-            return $this->statement->fetchAll(PDO::FETCH_OBJ);
-        }
-
-        public function single()
-        {
-            $this->execute();
-            return $this->statement->fetch(PDO::FETCH_OBJ);
-        }
-
-        public function rowCount()
-        {
-            return $this->statement->rowCount();
-        }
+        $this->statement->bindValue($param, $value, $type);
     }
+
+    public function execute()
+    {
+        return $this->statement->execute();
+    }
+    public function resultSet()
+    {
+        $this->execute();
+        return $this->statement->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function single()
+    {
+        $this->execute();
+        return $this->statement->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function rowCount()
+    {
+        return $this->statement->rowCount();
+    }
+}
