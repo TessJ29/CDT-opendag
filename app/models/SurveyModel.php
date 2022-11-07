8125ac001b4711d70708a30fc5ba2ee07cf07a5f
Tess Jansen <tess.jansen29@gmail.com>
2022-11-07 14:22:27 +0100

    survey model

--- app/models/SurveyModel.php
+++ app/models/SurveyModel.php
@@ -11,29 +11,27 @@ class SurveyModel
 
     public function getQuestions()
     {
-        $this->db->query("SELECT * FROM `Survey`");
+        $this->db->query("SELECT * FROM `Question`");
         $result = $this->db->resultSet();
         return $result;
     }
 
     public function getSingleQuestion($id)
     {
-        $this->db->query("SELECT * FROM Survey WHERE Id = :id");
+        $this->db->query("SELECT * FROM Question WHERE Id = :id");
         $this->db->bind(':id', $id, PDO::PARAM_INT);
         return $this->db->single();
     }
 
-    public function updateCountry($post)
+    public function updateAnswer($post)
     {
         // var_dump($post);
         // exit();
-        $this->db->query("UPDATE `Survey`
+        $this->db->query("UPDATE `User_answer`
                           SET Answer = :Answer,
-                              Description = :Description,
                           WHERE Id = :id");
 
         $this->db->bind(':Answer', $post["Answer"], PDO::PARAM_STR);
-        $this->db->bind(':Description', $post["Description"], PDO::PARAM_STR);
         $this->db->bind(':id', $post["Id"], PDO::PARAM_INT);
 
 
