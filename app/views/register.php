7c0be08c8cb9c05514bf83a255189512ed030cb0
[ClaudiaNijholt] <[325184@student.mboutrecht.nl]>
2022-11-08 15:10:53 +0100

    Navbar Icons and Logo fixed

--- app/views/register.php
+++ app/views/register.php
@@ -13,69 +13,7 @@
 
         <!-- Navigation bar -->
 
-        <header>
-            <nav>
-                <div class="top__navbar">
-                    <ul class="navbar__logo">
-                        <li><img src="/public/img/mbologo.png" alt=""></li>
-                    </ul>
-                    <div class="navbar__toggle" id="mobile-menu">
-                            <span class="bar"></span>
-                            <span class="bar"></span>
-                            <span class="bar"></span>
-                    </div>
-                    <ul class="navbar__contact--links">
-                        <li>
-                            <ion-icon name="mail-outline"></ion-icon>                
-                            <a href="">info@mboutrecht.nl</a>
-                        </li>
-                        <li>
-                            <ion-icon name="logo-whatsapp"></ion-icon>
-                            <a href="">06 - 257 050 51 (WhatsApp)</a>
-                        </li>
-                    </ul>
-                    <ul class="navbar__media--links">
-                        <li>
-                            <a class="navbar__media--link" href=""><ion-icon name="logo-linkedin"></ion-icon></a>
-                        </li>
-                        <li>
-                            <a class="navbar__media--link" href=""><ion-icon name="logo-twitter"></ion-icon></a>
-                        </li>
-                        <li>
-                            <a class="navbar__media--link" href=""><ion-icon name="logo-youtube"></ion-icon></a>
-                        </li>
-                        <li>
-                            <a class="navbar__media--link" href=""><ion-icon name="logo-instagram"></ion-icon></a>
-                        </li>
-                        <li>
-                            <a class="navbar__media--link" href=""><ion-icon name="logo-facebook"></ion-icon></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="bottom__navbar">
-                    <ul>
-                        <li>
-                            <a class="active" href="./index.php">HOME</a>
-                        </li>
-                        <li>
-                            <a href="/about">OVER MBO UTRECHT</a>
-                        </li>
-                        <li>
-                            <a href="/course">OPLEIDINGEN</a>
-                        </li>
-                        <li>
-                            <a href="/workshop">VOORLICHTINGEN</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <button>
-                            <a href="/login">Inloggen</a>
-                            <ion-icon name="arrow-forward-outline"></ion-icon>
-                        </button>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        <?php $this->view("partials/navbar"); ?>
 
         <!-- Content -->
         
