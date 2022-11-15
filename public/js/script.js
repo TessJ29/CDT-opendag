d6da5926b8afdcb25726bf387d67280a63921c69
Tess Jansen <tess.jansen29@gmail.com>
2022-11-15 14:40:41 +0100

    js to change data-steps on survey page

--- public/js/script.js
+++ public/js/script.js
@@ -11,3 +11,24 @@ const mobileMenu = () => {
 //* This line of code listens to when the hamburger-button is pressed.
 //* When it is, it will execute the mobileMenu function.
 menu.addEventListener('click', mobileMenu);
+
+
+
+// Survey
+// Switch between data steps
+
+const setStep = step => {
+    document.querySelectorAll(".step-content").forEach(element => element.style.display = "none");
+    document.querySelector("[data-step='" + step + "']").style.display = "block";
+    document.querySelectorAll(".steps .step").forEach((element, index) => {
+        index < step - 1 ? element.classList.add("complete") : element.classList.remove("complete");
+        index == step - 1 ? element.classList.add("current") : element.classList.remove("current");
+    });
+};
+document.querySelectorAll("[data-set-step]").forEach(element => {
+    element.onclick = event => {
+        event.preventDefault();
+        setStep(parseInt(element.dataset.setStep));
+    };
+});
+
